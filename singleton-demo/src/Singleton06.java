029e3bfc439b785db576fd1ae8218107e95750bd
houcheng <houcheng@boco.com.cn>
2022-03-03 22:11:53 +0800

    TODO

--- singleton-demo/src/Singleton06.java
+++ singleton-demo/src/Singleton06.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicReference;
  * @date ：Created in 2022/2/27 22:26
  * @modified ：
  * CAS「AtomicReference」(线程安全)
- * 但是会创建多个对象 TODO
+ * 但是会创建多个对象 TODO 如何破坏单例 反射/序列化反序列化 CAS 为啥会多次实例化，但是又能保证一个对象
  */
 public class Singleton06 {
 
